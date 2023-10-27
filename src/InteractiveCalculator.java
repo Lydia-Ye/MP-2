6f2ed5c569fe37711f7338ab651f1f6a1b838848
yezi <yezi@grinnell.edu>
2023-10-26 21:46:15 -0500

    Updated InteractiveCalculator

--- src/InteractiveCalculator.java
+++ src/InteractiveCalculator.java
@@ -10,29 +10,29 @@ import java.util.Scanner;
  */
 
 public class InteractiveCalculator {
-    public static void main(String[] args) throws Exception {
-        PrintWriter pen = new PrintWriter(System.out, true);
-        Scanner input = new Scanner(System.in);
-        pen.println(" Please enter the expression (enter QUIT to stop): ");
+  public static void main(String[] args) throws Exception {
+    PrintWriter pen = new PrintWriter(System.out, true);
+    Scanner input = new Scanner(System.in);
+    pen.println(" Please enter the expression (enter QUIT to stop): ");
         
-        BFCalculator calculator = new BFCalculator();
-        String exp = input.nextLine(); 
-        String[] expArray = exp.split("\\s+");
+    BFCalculator calculator = new BFCalculator();
+    String exp = input.nextLine(); 
+    String[] expArray = exp.split("\\s+");
       
-        while (!expArray[0].equals("QUIT")) {
-          if (expArray[0].equals("STORE")) {
-            calculator.store(expArray[1].charAt(0));
-          } else {
-            BigFraction result = calculator.evaluate(exp);
-            pen.println(result);
-          } // if
+    while (!expArray[0].equals("QUIT")) {
+      if (expArray[0].equals("STORE")) {
+        calculator.store(expArray[1].charAt(0));
+      } else {
+        BigFraction result = calculator.evaluate(exp);
+        pen.println(result);
+      } // if
 
-          exp = input.nextLine(); 
-          expArray = exp.split("\\s+");
-        } // while
+      exp = input.nextLine(); 
+      expArray = exp.split("\\s+");
+    } // while
 
-        pen.flush();
-        input.close();
-        return;
-    } // main
+    pen.flush();
+    input.close();
+    return;
+  } // main
 } // class InterativeCalculator
