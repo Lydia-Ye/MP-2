38fbe2b1f44c2acee75b2040e730c7d085da86c6
yezi <yezi@grinnell.edu>
2023-10-26 21:56:15 -0500

    Updated InteractiveCalculator with comments

--- src/InteractiveCalculator.java
+++ src/InteractiveCalculator.java
@@ -12,21 +12,30 @@ import java.util.Scanner;
 public class InteractiveCalculator {
   public static void main(String[] args) throws Exception {
     PrintWriter pen = new PrintWriter(System.out, true);
+
+    // Prompts user to enter expression
     Scanner input = new Scanner(System.in);
     pen.println(" Please enter the expression (enter QUIT to stop): ");
         
+    // Create a BF calculator
     BFCalculator calculator = new BFCalculator();
+
+    // Convert user's input into a string array
     String exp = input.nextLine(); 
     String[] expArray = exp.split("\\s+");
       
+    // Evaluate the input
     while (!expArray[0].equals("QUIT")) {
       if (expArray[0].equals("STORE")) {
+        // Store register
         calculator.store(expArray[1].charAt(0));
       } else {
+        // Evaluates numeric expression
         BigFraction result = calculator.evaluate(exp);
         pen.println(result);
       } // if
 
+      // Take new input until the user quit program
       exp = input.nextLine(); 
       expArray = exp.split("\\s+");
     } // while
